ddefa73a319f17343da73d3d7ee3a7d5d9010f99
Danh Le <dtle82@gmail.com>
2016-10-11 18:33:27 -0700

    calculator version0.5 - Danh Le

---
+++ script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by danh on 10/11/16.
+ */
+var my_calculator = new calculator(calc);
+var multipleSign = "*"; // definite your text for multiple
+
+function calc(type, value, item){
+    switch(value) {
+        case undefined:
+            $("#calc_skeleton .screen").text("");
+            break;
+        default:
+            $("#calc_skeleton .screen").text(value);
+            break;
+    }
+
+}
+
+function apply_click_handler() {
+    $("#calc_skeleton").on("click","button.btn",function() {
+        console.log($(this).text());
+        var val = $(this).text();
+        switch(val) {
+            case 'CE':
+                my_calculator.allClear();
+                break;
+            case 'C':
+                my_calculator.clear();
+                break;
+            case multipleSign:
+                my_calculator.addItem("x");
+                break;
+            default:
+                my_calculator.addItem(val);
+                break;
+        }
+
+    });
+}
+
+$(document).ready(apply_click_handler);
